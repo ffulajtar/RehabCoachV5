2be60adbf26c9d4d640f21adf2fa4896066d98bb
Ryan Gibson <rgibson518@gmail.com>
2014-03-24 21:45:01 -0400

    Added user login process

--- src/com/example/rehab_coachv1/CreateProfileActivity.java
+++ src/com/example/rehab_coachv1/CreateProfileActivity.java
@@ -1,7 +1,13 @@
 package com.example.rehab_coachv1;
 
+import com.example.rehab_coachv1.PersonContract.PersonDbHelper;
+import com.example.rehab_coachv1.PersonContract.PersonEntry;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 
 public class CreateProfileActivity extends Activity {
@@ -11,6 +17,39 @@ public class CreateProfileActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setTheme(android.R.style.Theme_Holo);
 		setContentView(R.layout.activity_create_profile);
+		PersonDbHelper mDbHelper = new PersonDbHelper(this.getBaseContext());
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+		
+		String id = "1";
+		String name = "Ryan Gibson";
+		
+		String address = "7854 Solitude Drive";
+		String hPhone = "3303283265";
+		String mPhone = "3303283265";
+		String email = "rgibson518@gmail.com";
+		String password = "password";
+		
+		ContentValues values = new ContentValues();
+		values.put(PersonEntry.COLUMN_NAME_PERSON_ID, id);
+		values.put(PersonEntry.COLUMN_NAME_NAME, name);
+		values.put(PersonEntry.COLUMN_NAME_ADDRESS, address);
+		values.put(PersonEntry.COLUMN_NAME_HOME_PHONE, hPhone);
+		values.put(PersonEntry.COLUMN_NAME_EMAIL, email);
+		values.put(PersonEntry.COLUMN_NAME_MOBILE_PHONE, mPhone);
+		values.put(PersonEntry.COLUMN_NAME_PASSWORD, password);
+
+		// Insert the new row, returning the primary key value of the new row
+		long newRowId;
+		newRowId = db.insert(
+		         PersonEntry.TABLE_NAME,
+		         null,
+		         values);
+		
+		System.out.println("Added user. Key == " + newRowId);
+		
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
+		finish();
 	}
 
 	@Override
--- src/com/example/rehab_coachv1/LoginActivity.java
+++ src/com/example/rehab_coachv1/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.rehab_coachv1;
 
+
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class LoginActivity extends Activity {
+
+	// Values for email and password at the time of the login attempt.
+	private String mEmail;
+	private String mPassword;
+	private String sPassword;
+
+	// UI references.
+	private EditText mEmailView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +33,107 @@ public class LoginActivity extends Activity {
 		setTheme(android.R.style.Theme_Holo);
 		setContentView(R.layout.activity_login_light);
 
-		
+		// Set up the login form.
+		mEmail = getIntent().getStringExtra("EMAIL");
+		sPassword = getIntent().getStringExtra("PASSWORD");
+		System.out.println(sPassword);
+		mEmailView = (EditText) findViewById(R.id.email);
+		mEmailView.setText(mEmail);
+
+		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView
+		.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int id,
+					KeyEvent keyEvent) {
+				if (id == R.id.login || id == EditorInfo.IME_NULL) {
+					attemptLogin();
+					return true;
+				}
+				return false;
+			}
+		});
+
+		mLoginFormView = findViewById(R.id.login);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		findViewById(R.id.login).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
 	}
+
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid email, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+		// Reset errors.
+		mEmailView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mEmail = mEmailView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(mEmail)) {
+			mEmailView.setError(getString(R.string.error_field_required));
+			focusView = mEmailView;
+			cancel = true;
+		} else if (!mEmail.contains("@")) {
+			mEmailView.setError(getString(R.string.error_invalid_email));
+			focusView = mEmailView;
+			cancel = true;
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			//showProgress(true);
+			//mAuthTask = new UserLoginTask();
+			//mAuthTask.execute((Void) null);
+			if (mPassword.compareTo(sPassword) == 0) {
+				//proceed to register activity
+				Intent intent = new Intent(this, HomeActivity.class);
+				startActivity(intent);
+				finish();
+			} else {
+				mPasswordView
+				.setError(getString(R.string.error_incorrect_password));
+				mPasswordView.requestFocus();
+			}
+
+
+		}
+	}
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,18 +141,18 @@ public class LoginActivity extends Activity {
 		//getMenuInflater().inflate(R.menu.login, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
-	
+
+
 	public void loginToApp(View view)
 	{
 		Intent home = new Intent(this, HomeActivity.class);
 		startActivity(home);
 	}
-	
+
 	public void adminMode(View view)
 	{
 		Intent admin = new Intent(this, AdminActivity.class);
 		startActivity(admin);
 	}
-	
+
 }
