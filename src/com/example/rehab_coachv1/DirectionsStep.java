69100a6ea1c28aefbdafa2b0e11c3247b6a7c273
Michael Boker <boker.6@osu.edu>
2014-04-12 17:34:17 -0400

    Mike Commit again to make sure all resources are in#	new file:   bin/classes/.DS_Store

---
+++ src/com/example/rehab_coachv1/DirectionsStep.java
@@ -0,0 +1,12 @@
+package com.example.rehab_coachv1;
+
+import android.location.Location;
+
+public class DirectionsStep {
+	public String HtmlInstructions;
+//	public String maneuver;
+	public String distance;
+	public String duration;
+	public Location startLocation;
+	public Location endLocation;
+}
